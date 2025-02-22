f2cbbee5d96857826befb4277f713299df4276fb
Animator55 <animator55developer@gmail.com>
2025-02-22 20:09:58 -0300

    added header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,38 @@
+import "../assets/header.css"
+
 export default function Header () {
 
-    return <header>
-        Header
+    return <header className="header-block" id="Book Now">
+        <h1>Explore Your Desired Hotel All Over The World</h1>
+        <section className="search-div">
+            <div className="input-container">
+                <p>Search</p>
+                <input autoComplete="address" placeholder="Type the place"/>
+            </div>
+            <div className="input-container">
+                <p>Check In</p>
+                <input type="date"/>
+            </div>
+            <div className="input-container">
+                <p>Check Out</p>
+                <input type="date"/>
+            </div>
+            <div className="input-container">
+                <p>Person</p>
+                <select>
+                    {[1,2,3,4].map(el=>{
+                        return <option key={Math.random()}>
+                            {el}
+                        </option>
+                    })}
+                </select>
+            </div>
+            <button>
+                Book Now
+            </button>
+        </section>
+        <div className="image-header">
+
+        </div>
     </header>
 }
