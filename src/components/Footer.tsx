c50c86769aaab77af79a3b232562d064f314e4dd
Animator55 <animator55developer@gmail.com>
2025-02-22 19:24:55 -0300

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,43 +1,44 @@
 import { faMapMarker, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../assets/footer.css"
 
 export default function Footer () {
-    return <footer id="Contact">
-        <section>
+    return <footer id="Contact" className="block">
+        <section className="footer">
 
-            <div>
+            <div className="footer-column">
                 <h3>Logo</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Magni itaque dolore nemo, illum id optio tempore velit totam.</p>
-                <div>
-                </div>
+                {/* <div>
+                </div> */}
             </div>
-            <div>
+            <div className="footer-column">
                 <h2>About</h2>
                 <p>Home</p>
                 <p>Terms</p>
                 <p>Privacy & Policy</p>
                 <p>Finance</p>
             </div>
-            <div>
+            <div className="footer-column">
                 <h2>Support</h2>
                 <p>Support</p>
                 <p>Service</p>
                 <p>Blog</p>
                 <p>Customer</p>
             </div>
-            <div>
+            <div className="footer-column">
                 <h2>Contact Us</h2>
-                <p>
+                <div>
                     <FontAwesomeIcon icon={faMapMarker}/>
-                    Random, Ubication, Some Country
-                </p>
-                <p>
+                    <p>Random, Ubication, Some Country</p>
+                </div>
+                <div>
                     <FontAwesomeIcon icon={faPhone}/>
-                    +1 67 482 483
-                </p>
+                    <p>+1 67 482 483</p>
+                </div>
             </div>
         </section>
-        <p>All Rights Reserved by Nahuel Ibarra</p>
+        <p className="paragraph">All Rights Reserved by Nahuel Ibarra</p>
     </footer>
 }
