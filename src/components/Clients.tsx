ead39dc8de748e0df55f4ae2a615c784f26dbf93
Animator55 <animator55developer@gmail.com>
2025-02-22 12:34:26 -0300

    adding carrousel wip

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,42 +1,104 @@
 import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
+import "../assets/clients.css"
 
-export default function Clients () {
-    const [carrouselIndex, setIndex ]= React.useState(0)
+export default function Clients() {
+    const [carrouselIndex, setIndex] = React.useState(0)
 
-    const clientsList: {image: any, quote: string, name: string, description: string}[] = [
-        {image: "red", quote: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid et incidunt libero magni! Fugit officiis sapiente nesciunt sint dolorem, laudantium unde, necessitatibus architecto repellat sit vitae odit eum reiciendis.", name:"Ifdhisnf ianfam", description: "Drevl CEO"},
-        {image: "red", quote: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid et incidunt libero magni! Fugit officiis sapiente nesciunt sint dolorem, laudantium unde, necessitatibus architecto repellat sit vitae odit eum reiciendis.", name:"Ifdhisnf ianfam", description: "Drevl CEO"},
-        {image: "red", quote: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid et incidunt libero magni! Fugit officiis sapiente nesciunt sint dolorem, laudantium unde, necessitatibus architecto repellat sit vitae odit eum reiciendis.", name:"Ifdhisnf ianfam", description: "Drevl CEO"},
-        {image: "red", quote: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid et incidunt libero magni! Fugit officiis sapiente nesciunt sint dolorem, laudantium unde, necessitatibus architecto repellat sit vitae odit eum reiciendis.", name:"Ifdhisnf ianfam", description: "Drevl CEO"},
-        {image: "red", quote: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid et incidunt libero magni! Fugit officiis sapiente nesciunt sint dolorem, laudantium unde, necessitatibus architecto repellat sit vitae odit eum reiciendis.", name:"Ifdhisnf ianfam", description: "Drevl CEO"},
-        {image: "red", quote: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid et incidunt libero magni! Fugit officiis sapiente nesciunt sint dolorem, laudantium unde, necessitatibus architecto repellat sit vitae odit eum reiciendis.", name:"Ifdhisnf ianfam", description: "Drevl CEO"},
-    ]
-    const QuoteItem = ({index}: {index: number})=>{
+    const clientsList: { image: any, quote: string, name: string, description: string }[] = [
+        {
+            image: "red",
+            quote: "The service was absolutely impeccable. I felt like royalty from the moment I stepped in.",
+            name: "Emily Carter",
+            description: "Frequent traveler, New York"
+        },
+        {
+            image: "blue",
+            quote: "A truly relaxing stay! The rooms were spotless, and the ocean view was breathtaking.",
+            name: "James Holden",
+            description: "Business consultant, London"
+        },
+        {
+            image: "green",
+            quote: "The staff went above and beyond to make our honeymoon unforgettable. We'll be back for sure!",
+            name: "Sophia Martinez",
+            description: "Newlywed, Madrid"
+        },
+        {
+            image: "orange",
+            quote: "I travel a lot for work, and this hotel stands out for its comfort and efficiency. Highly recommended!",
+            name: "Daniel Thompson",
+            description: "Tech entrepreneur, San Francisco"
+        },
+        {
+            image: "yellow",
+            quote: "Best sleep I've had in months! The bed was so comfortable, and the atmosphere was so peaceful.",
+            name: "Laura Chen",
+            description: "Writer, Melbourne"
+        },
+        {
+            image: "lime",
+            quote: "The breakfast buffet was divine. Fresh ingredients, amazing variety, and great coffee!",
+            name: "Marco Rossi",
+            description: "Food critic, Rome"
+        },
+        {
+            image: "pink",
+            quote: "I've stayed in many luxury hotels, but this one felt like home. The warmth of the staff made all the difference.",
+            name: "Isabelle Dupont",
+            description: "Fashion designer, Paris"
+        },
+        {
+            image: "black",
+            quote: "Location is perfect, right in the heart of the city. Walkable distance to all major attractions.",
+            name: "Samuel Kim",
+            description: "Tourist, Seoul"
+        },
+        {
+            image: "gray",
+            quote: "An absolute gem! The spa services were top-notch, and I left feeling completely rejuvenated.",
+            name: "Olivia White",
+            description: "Wellness coach, Toronto"
+        },
+        {
+            image: "cyan",
+            quote: "I felt incredibly safe and well taken care of throughout my stay. Perfect for solo travelers!",
+            name: "Ethan Walker",
+            description: "Solo adventurer, Cape Town"
+        }
+    ];
+
+    const QuoteItem = ({ index }: { index: number }) => {
         const quoteData = clientsList[index]
-        return <div>
+        return <div className="quote-item">
             {/* <img/> */}
-            <div style={{background: quoteData.image}}></div>
-            <p>{quoteData.quote}</p>
-            <h6>{quoteData.name}</h6>
-            <p>{quoteData.description}</p>
+            <div style={{ background: quoteData.image }}></div>
+            <div className="quote-content">
+                <p>{quoteData.quote}</p>
+                <h6>{quoteData.name}</h6>
+                <p className="title">{quoteData.description}</p>
+            </div>
         </div>
     }
-    const prevIndex =carrouselIndex -1 === -1 ? (clientsList.length -1) : carrouselIndex
-    const nexIndex =carrouselIndex +1 === clientsList.length ? 0 : carrouselIndex
+    const prevIndex = carrouselIndex - 1 === -1 ? (clientsList.length - 1) : (carrouselIndex - 1)
+    const prevprevIndex = prevIndex - 1 === -1 ? (clientsList.length - 1) : (prevIndex - 1)
+    const nexIndex = carrouselIndex + 1 === clientsList.length ? 0 : (carrouselIndex + 1)
+    const nexnexIndex = nexIndex + 1 === clientsList.length ? 0 : (nexIndex + 1)
 
-    return <section>
+    return <section className="block">
         <h4>What our Clients say</h4>
-        <div>
-            <button onClick={()=>{setIndex(carrouselIndex-1)}}>
-                <FontAwesomeIcon icon={faCaretLeft}/>
+        <div className="carrousel">
+            <button onClick={() => { setIndex(prevIndex) }}>
+                <FontAwesomeIcon icon={faCaretLeft} />
             </button>
-            <QuoteItem index={prevIndex}/>
-            <QuoteItem index={carrouselIndex}/>
-            <QuoteItem index={nexIndex}/>
-            <button onClick={()=>{setIndex(carrouselIndex+1)}}>
-                <FontAwesomeIcon icon={faCaretRight}/>
+            <QuoteItem index={prevprevIndex} />
+            <QuoteItem index={prevIndex} />
+            <QuoteItem index={carrouselIndex} />
+            <QuoteItem index={nexIndex} />
+            <QuoteItem index={nexnexIndex} />
+            <button onClick={() => { setIndex(nexIndex) }}>
+                <FontAwesomeIcon icon={faCaretRight} />
             </button>
         </div>
     </section>
